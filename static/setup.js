e9d8235892e5311858a5d192dd22925a5e1104f2
ADMARIl <aings1@umbc.edu>
2019-03-10 18:19:55 -0400

    better headshot and a few other things

--- static/setup.js
+++ static/setup.js
@@ -1,12 +1,14 @@
 /*
     Sync the header and footer so everything looks n i c e
-*/$(() => {
+*/
+$(() => {
     /* Load the header */
     $("div#header").load("components/header.html", () => {
         /* Once it's loaded, fade it smoothly in */
        // enableScrollFeature()
         console.log( "header ready!" );
     })
+    /* Load the footer */
     $("div#footer").load("components/footer.html", () => {
         /* Once it's loaded, fade it smoothly in */
         //enableScrollFeature()
