a8e8ea3e7a779b2bc852769a78e1a36800f8b882
ADMARIl <aings1@umbc.edu>
2019-03-09 23:54:30 -0500

    Made all the pages work with header and footer syncing

--- static/setup.js
+++ static/setup.js
@@ -2,6 +2,12 @@ $(() => {
     /* Load the header */
     $("div#header").load("components/header.html", () => {
         /* Once it's loaded, fade it smoothly in */
-        enableScrollFeature()
+       // enableScrollFeature()
+        console.log( "header ready!" );
+    })
+    $("div#footer").load("components/footer.html", () => {
+        /* Once it's loaded, fade it smoothly in */
+        //enableScrollFeature()
+        console.log( "footer ready!" );
     })
 })
