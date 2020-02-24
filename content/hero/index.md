be184eff96e6109228acb743887b759f2a56d0d6
Andrew Ingson <aings1@umbc.edu>
2020-02-24 18:17:25 -0500

    minor changes

---
+++ content/hero/index.md
@@ -0,0 +1,8 @@
+---
+title: 'Hi, my name is'
+name: 'Andrew Ingson'
+subtitle: 'A human person.'
+contactText: 'Get In Touch'
+---
+
+I'm an undergraduate student at the University of Maryland, Baltimore County, where I am majoring in Computer Science. Right now, I'm looking for a technical internship in the field of Software Engineering
---
+++ old/static/setup.js
@@ -0,0 +1,24 @@
+/*
+    Sync the header and footer so everything looks n i c e
+*/
+
+$(() => {
+  /*$("div#nest1").load(function() {
+        $('.preloader').fadeOut('slow');
+        console.log("fade out!");
+     });*/
+  /* Load the header */
+  $('div#header').load('components/header.html', () => {
+    /* Once it's loaded, fade it smoothly in */
+    // enableScrollFeature()
+    console.log( 'header ready!' );
+  });
+  /* Load the footer */
+  $('div#footer').load('components/footer.html', () => {
+    /* Once it's loaded, fade it smoothly in */
+    //enableScrollFeature()
+    console.log( 'footer ready!' );
+  });
+
+   
+});
