f8fa286680e9050c3780b6f05547aa1305587fc2
Andrew Ingson <aings1@umbc.edu>
2021-06-15 01:41:33 -0400

    Added typewriter effect

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { email } from '@config';
 import styled from 'styled-components';
-import { theme, mixins, media, Section } from '@styles';
+import { media, mixins, Section, theme } from '@styles';
+import Typewriter from 'typewriter-effect';
+
 const { colors, fontSizes, fonts } = theme;
 
 const StyledContainer = styled(Section)`
@@ -23,13 +25,12 @@ const HelloContainerRed = styled(Section)`
   ${media.tablet`width: 410px;`};
   ${media.phablet`width: 375px;`};
   ${media.phone`width: 280px;`};
-  margin: 0px 0px 20px -9px;
+  margin: 0 0 0px -9px;
   padding: 20px 50px 20px 20px !important;
-  margin-bottom: 0px;
 `;
 const HelloContainerwhite = styled(Section)`
   background-color: ${colors.white};
-  margin: 0px 0px 10px -9px;
+  margin: 0 0 10px -9px;
   padding: 20px 50px 5px 10px;
   ${media.desktop`
     width: 500px;
@@ -109,7 +110,19 @@ const Hero = ({ data }) => {
     </StyledTitle>
   );
   const three = () => (
-    <StyledSubtitle style={{ transitionDelay: '300ms' }}>{frontmatter.subtitle}</StyledSubtitle>
+    <StyledSubtitle style={{ transitionDelay: '300ms' }}>
+      <div style={{ flexDirection: 'row' }}>
+        I <Typewriter
+          options={{
+            strings: ['code', 'design', 'create', 'develop'],
+            autoStart: true,
+            loop: true,
+            pauseFor: 1500,
+          }}
+          style={{ flexDirection: 'row' }}
+        /> things
+      </div>
+    </StyledSubtitle>
   );
   const four = () => (
     <StyledDescription
